cb2f7c3b252a15c35af71f6ec70da2e7d6760751
Angel Busto <bustoo52@gmail.com>
2023-03-30 18:03:30 -0300

    Wood-Design v2.0

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+//EFECTO SCROLL
+let progress = document.getElementById('progress_bar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
